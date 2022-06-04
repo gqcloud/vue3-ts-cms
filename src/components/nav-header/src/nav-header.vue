ca58ad49160fb8d5ae6c7399003a024920717e76
gqcloud <874310491@qq.com>
2022-06-04 18:42:24 +0800

    feat(主页侧边栏): 主页侧边栏ui及部分联动效果

---
+++ src/components/nav-header/src/nav-header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="nav-header">
+    <el-icon class="fold-menu" v-if="isFold" @click="handleFoldClick"
+      ><Fold
+    /></el-icon>
+    <el-icon class="fold-menu" v-else @click="handleFoldClick"
+      ><Expand
+    /></el-icon>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+export default defineComponent({
+  setup(props, { emit }) {
+    const isFold = ref(false)
+    const handleFoldClick = () => {
+      isFold.value = !isFold.value
+      emit("foldChange", isFold.value)
+    }
+    return {
+      isFold,
+      handleFoldClick
+    }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.nav-header {
+  .fold-menu {
+    font-size: 30px;
+    cursor: pointer;
+  }
+}
+</style>
